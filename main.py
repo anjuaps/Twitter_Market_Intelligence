f3341cd776b5e9bcf386980d4c415ba88b381840
Anju <117897598+anjuaps@users.noreply.github.com>
2026-01-14 22:38:20 +0530

    Added Twitter scraping and parquet writing features
    Integrate Twitter scraping and storage functionality.

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from processing.cleaner import clean_text
 from processing.deduplicator import TweetDeduplicator
-
+from scraper.twitter_scraper import scrape_all_hashtags
+from storage.parquet_writer import write_tweets_to_parquet
 
 def main():
     print("Starting Twitter Market Intelligence System")
-    raw_tweet= "🚀 BUY BankNifty breakout now! https://t.co/test"
-
-    cleaned_tweet= clean_text(raw_tweet)
-    print("Cleaned Tweet:", cleaned_tweet)
 
     deduplicator= TweetDeduplicator()
-    tweet_id= hash(cleaned_tweet)
+    processed= []
 
-    if deduplicator.is_duplicate(str(tweet_id)):
-        print("Duplicate tweet detected")
-    else:
-        print("New tweet processed")
+    tweets= scrape_all_hashtags()
 
-if __name__== "__main__":
+    for tweet in tweets:
+        cleaned= clean_text(tweet["content"])
+        tweet_id= hash(tweet["username"]+cleaned+tweet["timestamp"])
+
+        if not deduplicator.is_duplicate(str(tweet_id)):
+            tweet["content"]=cleaned
+            processed.append(tweet)
+
+    write_tweets_to_parquet(processed)
+
+if __name__ == "__main__":
     main()
-
-
